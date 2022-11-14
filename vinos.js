ae8a9b6ed2cb1bd4f1175e0f65e14180ae524aab
Adriana Solorzano <[200~adriana198930@gmail.com>
2022-11-14 20:25:26 -0300

    final

---
+++ vinos.js
@@ -0,0 +1,14 @@
+let stockProductos = [
+    {id: 1, nombre: "Vino Malbec Cordoba", cantidad: 1, precio: 2500, img: './img/PREMIUM-MALBEC.PNG class="imagen1"'},
+    {id: 2, nombre: "Vino Cabernet Franc El Gran Enemigo", cantidad: 1, precio: 4650, img: './img/gran-enemigo.png class="imagen2"'},
+    {id: 3, nombre: "Vino Malbec Reserva Bag", cantidad: 1 , precio: 3500, img: './img/reservabag.jpg class="imagen3"'},
+    {id: 4, nombre: "Vino Mosquita Muerta", cantidad: 1, precio: 4500, img: './img/mm.jpeg class="imagen4"'},
+    {id: 5, nombre: "Vino Saint Felicien Malbec", cantidad: 1, precio: 2500, img: './img/SaintFelicien.jpeg class="imagen5"'},
+    {id: 6, nombre: "Cabernet Rutini", cantidad: 1, precio: 3500, img: './img/rutini.jpeg class="imagen6"'},
+    {id: 7, nombre: "Vino Blanco Gata Flora Torrentes", cantidad: 1, precio: 1500, img: './img/torrentes.jpg class="imagen7"'},
+    {id: 8, nombre: "Vino Vistades Gradum Carmenere", cantidad: 1,  precio: 2800, img: './img/gradum-carmenere.png class="imagen8"'},
+    {id: 9, nombre: "Vino Cabernet Franc Mestizo", cantidad: 1, precio: 2500, img: './img/mestizo.jpg class="imagen9"'},
+    {id: 10, nombre: "Vino Cabernet Ernesto Catena", cantidad: 1, precio:4800, img: './img/ernesto.jpg class="imagen10"'},
+    {id: 11, nombre: "Vino RedBlend Andeluna Elevación", cantidad: 1, precio: 4500, img: './img/andeluna-elevacion.jpg class="imagen11"'},
+    {id: 12, nombre: "Vino RedBlend Cocodrilo", cantidad: 1, precio: 2500, img: './img/cocodrilo.jpg class="imagen12"'},
+]
